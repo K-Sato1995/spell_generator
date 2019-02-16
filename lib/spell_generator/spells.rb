15471deb0190690dfc3dfe4071b8828a0e3be84f
Katsuki <Katsuki_Sato0130@icloud.com>
2019-02-16 15:06:52 +0900

    Add more spells;

--- lib/spell_generator/spells.rb
+++ lib/spell_generator/spells.rb
@@ -1,2 +1,7 @@
-SPELL_SET1 = %w(accurate sufficient ugly useful immediate entire healthy hot efficient dramatic)
-SPELL_SET2 = %w(punch kick attack blow smash strike smack cut poke stab)
+SPELL_SET1 = %w[accurate sufficient ugly useful immediate entire healthy hot efficient
+                dramatic threatening brave greedy racial regular glorious pretty alcoholic
+                instinctive symptomatic merciful cute perpetual average astonishing shallow
+                shallow hateful sacred divine].freeze
+
+SPELL_SET2 = %w[fire thunder attack blow smash strike smack cut poke stab punch kick kiss
+                flame darkness storm favor jump step hunt].freeze
