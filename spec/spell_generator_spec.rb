384346e58d943c901351c32da6e23ed9b3b1e0fe
Katsuki <Katsuki_Sato0130@icloud.com>
2019-02-16 14:58:39 +0900

    Added tests

--- spec/spell_generator_spec.rb
+++ spec/spell_generator_spec.rb
@@ -2,8 +2,15 @@ RSpec.describe SpellGenerator do
   it "has a version number" do
     expect(SpellGenerator::VERSION).not_to be nil
   end
+  describe '.generate' do
+    it 'creates a spell' do
+      expect(SpellGenerator::Generator.generate).to match(/\w+\s\w+/)
+    end
+  end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#generate' do
+    it 'creates a spell' do
+      expect(SpellGenerator::Generator.new.generate).to match(/\w+\s\w+/)
+    end
   end
 end
