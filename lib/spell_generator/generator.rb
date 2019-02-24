a4fc094d69252840bf0694648ba90c93e8704102
Katsuki <Katsuki_Sato0130@icloud.com>
2019-02-24 11:09:42 +0900

    Make class method simpler

--- lib/spell_generator/generator.rb
+++ lib/spell_generator/generator.rb
@@ -5,12 +5,8 @@ class SpellGenerator::Generator
   SPELL_SET1_NUMBER = SPELL_SET1.size
   SPELL_SET2_NUMBER = SPELL_SET2.size
 
-  class << self
-    def generate
-      spell_part1 = SPELL_SET1[rand(0...SPELL_SET1_NUMBER)]
-      spell_part2 = SPELL_SET2[rand(0...SPELL_SET2_NUMBER)]
-      "#{spell_part1} #{spell_part2}"
-    end
+  def self.generate
+    new.generate
   end
 
   def generate
