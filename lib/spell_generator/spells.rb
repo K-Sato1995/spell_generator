ffa344a69198b30ff65ca9141d3033e673ef64bc
Peter Witham <peter.witham@gmail.com>
2019-04-24 02:50:39 -0500

    Added a new spell.
    Added Amazing and Slap.

--- lib/spell_generator/spells.rb
+++ lib/spell_generator/spells.rb
@@ -1,7 +1,7 @@
 SPELL_SET1 = %w[accurate sufficient ugly useful immediate entire healthy hot efficient
                 dramatic threatening brave greedy racial regular glorious pretty alcoholic
                 instinctive symptomatic merciful cute perpetual average astonishing shallow
-                shallow hateful sacred divine].freeze
+                shallow hateful sacred divine amazing].freeze
 
 SPELL_SET2 = %w[fire thunder attack blow smash strike smack cut poke stab punch kick kiss
-                flame darkness storm favor jump step hunt].freeze
+                flame darkness storm favor jump step hunt slap].freeze
