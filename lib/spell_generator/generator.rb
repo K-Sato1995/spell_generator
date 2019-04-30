980b4ba82e61fea0c8e5aa634f9af6e8d10ddc2e
Jonathon Pemberton <jonathon@judson.biz>
2019-04-30 07:58:34 -0500

    Rename constants for clarity

--- lib/spell_generator/generator.rb
+++ lib/spell_generator/generator.rb
@@ -2,16 +2,13 @@ require_relative 'spells'
 require_relative 'spell_generator_error'
 
 class SpellGenerator::Generator
-  SPELL_SET1_NUMBER = SPELL_SET1.size
-  SPELL_SET2_NUMBER = SPELL_SET2.size
-
   def self.generate
     new.generate
   end
 
   def generate
-    spell_part1 = SPELL_SET1[rand(0...SPELL_SET1_NUMBER)]
-    spell_part2 = SPELL_SET2[rand(0...SPELL_SET2_NUMBER)]
+    spell_part1 = SPELL_ADJECTIVES[rand(0...SPELL_ADJECTIVES.size)]
+    spell_part2 = SPELL_VERBS[rand(0...SPELL_VERBS.size)]
     "#{spell_part1} #{spell_part2}"
   end
 
