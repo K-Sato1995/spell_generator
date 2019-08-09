f4a6db74884468560ebe79f4793de8287c011013
dalmo3 <33860060+dalmo3@users.noreply.github.com>
2019-08-09 14:35:12 +1200

    Update spells.rb

--- lib/spell_generator/spells.rb
+++ lib/spell_generator/spells.rb
@@ -3,10 +3,10 @@ SPELL_ADJECTIVES = %w[abandoned accurate adorable alcoholic amazing astonishing 
                 glorious greedy hateful healthy hot immediate instinctive lame magnificent 
                 marvelous merciful perpetual poisonous pretty quirky racial regular sacred 
                 shallow sickening sufficient symptomatic terrific threatening tyrannical ugly 
-                useful zany].freeze
+                useful zany malevolent outrageous].freeze
 
 SPELL_VERBS = %w[accept attach attack bet bind blow burn buy carry choose chop commit curse cut darkness 
                 decide doubt explain favor feed fire flame flood frost get give hunt improve 
                 include inform invest jump kick kiss lift love maintain manage organize overcome 
                 paint peel plan poke possess pounce pretend pull punch read realize slap smack 
-                smash stab step storm strike thunder tornado typhoon vortex wish write].freeze
+                smash stab step storm strike thunder tornado typhoon vortex wish write hover freefall].freeze
